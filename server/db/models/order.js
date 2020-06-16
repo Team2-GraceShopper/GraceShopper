4c54b06b2aa67272770797cd95e8704878b49118
nora <norakrantz1@gmail.com>
2020-06-16 13:40:19 -0400

    model setup

---
+++ server/db/models/order.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  orderDate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  subtotal: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  total: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  shipStreet: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  shipCity: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  shipState: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  shipZip: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  cardNumber: {
+    type: Sequelize.INTEGER,
+    validate: {
+      isCreditCard: true,
+      len: [16]
+    }
+  }
+})
+
+module.exports = Order
