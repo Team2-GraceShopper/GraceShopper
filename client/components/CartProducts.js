91cf574cbcb368461f95c032f27534f75e938e7e
shelilevine <shelisteinberg@gmail.com>
2020-06-17 13:28:23 -0400

    added images to shopping cart view

---
+++ client/components/CartProducts.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Grid from '@material-ui/core/Grid'
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import GridListTile from '@material-ui/core/GridListTile';
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+
+import {connect} from 'react-redux'
+
+// const products = [
+//   {name: 'Product 1', desc: 'A nice thing', price: '$9.99'},
+//   {name: 'Product 2', desc: 'Another thing', price: '$3.45'},
+//   {name: 'Product 3', desc: 'Something else', price: '$6.51'},
+//   {name: 'Product 4', desc: 'Best thing of all', price: '$14.11'},
+//   {name: 'Shipping', desc: '', price: 'Free'}
+// ]
+
+const products = [
+  {
+    productId: 1,
+    name: 'Awesome Soft Towels',
+    price: '247.00',
+    description: 'Atque et quas laudantium impedit iste.',
+    imageUrl: 'http://lorempixel.com/640/480/sports',
+    inventory: 84,
+    quantity: 2
+  },
+  {
+    productId: 5,
+    name: 'Sleek Cotton Hat',
+    price: '586.00',
+    description: 'Debitis harum placeat ut debitis quis modi aut.',
+    imageUrl: 'http://lorempixel.com/640/480/nature',
+    inventory: 89,
+    quantity: 4
+  },
+  {
+    productId: 3,
+    name: 'Handmade Plastic Computer',
+    price: '343.00',
+    description:
+      'Nihil eligendi adipisci voluptatem culpa ipsum quis iusto sunt eius.',
+    imageUrl: 'http://lorempixel.com/640/480/abstract',
+    inventory: 75,
+    quantity: 1
+  },
+  {
+    productId: 7,
+    name: 'Rustic Soft Shirt',
+    price: '785.00',
+    description: 'Asperiores est esse corporis dicta.',
+    imageUrl: 'http://lorempixel.com/640/480/food',
+    inventory: 59,
+    quantity: 3
+  },
+  {
+    productId: 2,
+    name: 'Awesome Rubber Mouse',
+    price: '710.00',
+    description: 'Maiores omnis deserunt eos ut alias sed eius maxime.',
+    imageUrl: 'http://lorempixel.com/640/480/food',
+    inventory: 56,
+    quantity: 1
+  },
+  {
+    productId: 66,
+    name: 'Gorgeous Cotton Tuna',
+    price: '880.00',
+    description: 'In minus quia maxime iusto inventore.',
+    imageUrl: 'http://lorempixel.com/640/480/abstract',
+    inventory: 60,
+    quantity: 1
+  }
+]
+// const addresses = ['1 Material-UI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+// const payments = [
+//   { name: 'Card type', detail: 'Visa' },
+//   { name: 'Card holder', detail: 'Mr John Smith' },
+//   { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+//   { name: 'Expiry date', detail: '04/2024' },
+// ];
+
+const useStyles = makeStyles(theme => ({
+  listItem: {
+    padding: theme.spacing(1, 1)
+    // border: "1px solid black",
+  },
+  total: {
+    fontWeight: 700
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    textAlign: 'center'
+  },
+  image: {
+    height: 150,
+    width: 200,
+    marginRight: 10
+  }
+}))
+
+export default function CartProducts() {
+  const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom className={classes.title}>
+        Shopping Cart
+      </Typography>
+      <List disablePadding>
+        {products.map(product => (
+          <ListItem className={classes.listItem} key={product.name}>
+            <img src={product.imageUrl} className={classes.image} />
+            {/* <ListItemAvatar className={classes.image}>
+               <img src={product.imageUrl} />
+            </ListItemAvatar> */}
+            <ListItemText
+              primary={product.name}
+              secondary={product.description}
+            />
+            <Typography variant="body2">{product.price}</Typography>
+          </ListItem>
+        ))}
+        <ListItem className={classes.listItem}>
+          <ListItemText primary="Total" />
+          <Typography variant="subtitle1" className={classes.total}>
+            $34.06
+          </Typography>
+        </ListItem>
+      </List>
+      <Grid container spacing={2}>
+        {/* <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom className={classes.title}>
+            Shipping
+          </Typography>
+          <Typography gutterBottom>John Smith</Typography>
+          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom className={classes.title}>
+            Payment details
+          </Typography>
+          <Grid container>
+            {payments.map((payment) => (
+              <React.Fragment key={payment.name}>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.name}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.detail}</Typography>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid> */}
+      </Grid>
+    </React.Fragment>
+  )
+}
--- client/store/cart.js
+++ client/store/cart.js
@@ -36,141 +36,70 @@ export default function(state = initialCart, action) {
 
 //EXAMPLE OUTPUT FROM AXIOS GET REQUEST
 // {
-//   "id": 101,
-//   "email": "jake@jake.com",
-//   "orderDate": "2019-06-21T16:43:39.755Z",
-//   "subtotal": "880.00",
-//   "tax": "3",
-//   "total": "75.00",
-//   "shipStreet": "305 Koepp Station",
-//   "shipCity": "Harbermouth",
-//   "shipState": "Rhode Island",
-//   "shipZip": 71788,
-//   "cardNumber": null,
-//   "cardExpiration": null,
-//   "cvvCode": null,
-//   "billStreet": "2199 Luettgen Grove",
-//   "billCity": "Santosburgh",
-//   "billState": "New Mexico",
-//   "billZip": 56663,
-//   "status": "active",
-//   "createdAt": "2020-06-16T22:54:14.392Z",
-//   "updatedAt": "2020-06-16T22:54:14.392Z",
-//   "userId": 52,
-//   "products": [
+//   "orderId": 101,
+//   "cart": [
 //       {
-//           "id": 1,
-//           "name": "Tasty Granite Soap",
-//           "price": "922.00",
-//           "description": "Consequatur molestias et quis beatae possimus voluptatem expedita est et.",
-//           "imageUrl": "http://lorempixel.com/640/480/technics",
-//           "inventory": 34,
-//           "createdAt": "2020-06-16T22:54:13.689Z",
-//           "updatedAt": "2020-06-16T22:54:13.689Z",
-//           "categoryId": 4,
-//           "OrderDetail": {
-//               "quantity": 3,
-//               "price": "518.00",
-//               "createdAt": "2020-06-16T22:54:15.073Z",
-//               "updatedAt": "2020-06-16T22:54:15.073Z",
-//               "orderId": 101,
-//               "productId": 1
-//           }
-//       },
-//       {
-//           "id": 5,
-//           "name": "Practical Fresh Pants",
-//           "price": "875.00",
-//           "description": "Fuga atque ipsum.",
-//           "imageUrl": "http://lorempixel.com/640/480/food",
-//           "inventory": 72,
-//           "createdAt": "2020-06-16T22:54:13.699Z",
-//           "updatedAt": "2020-06-16T22:54:13.699Z",
-//           "categoryId": 6,
-//           "OrderDetail": {
-//               "quantity": 3,
-//               "price": "815.00",
-//               "createdAt": "2020-06-16T22:54:15.073Z",
-//               "updatedAt": "2020-06-16T22:54:15.073Z",
-//               "orderId": 101,
-//               "productId": 5
-//           }
-//       },
-//       {
-//           "id": 3,
-//           "name": "Gorgeous Granite Soap",
-//           "price": "557.00",
-//           "description": "Nam odit nemo eligendi velit vel non doloremque.",
-//           "imageUrl": "http://lorempixel.com/640/480/abstract",
-//           "inventory": 98,
-//           "createdAt": "2020-06-16T22:54:13.695Z",
-//           "updatedAt": "2020-06-16T22:54:13.695Z",
-//           "categoryId": 3,
-//           "OrderDetail": {
-//               "quantity": 3,
-//               "price": "358.00",
-//               "createdAt": "2020-06-16T22:54:15.074Z",
-//               "updatedAt": "2020-06-16T22:54:15.074Z",
-//               "orderId": 101,
-//               "productId": 3
-//           }
-//       },
-//       {
-//           "id": 7,
-//           "name": "Incredible Fresh Salad",
-//           "price": "70.00",
-//           "description": "Molestiae dolor nisi reprehenderit quis adipisci.",
-//           "imageUrl": "http://lorempixel.com/640/480/abstract",
-//           "inventory": 43,
-//           "createdAt": "2020-06-16T22:54:13.703Z",
-//           "updatedAt": "2020-06-16T22:54:13.703Z",
-//           "categoryId": 6,
-//           "OrderDetail": {
-//               "quantity": 5,
-//               "price": "374.00",
-//               "createdAt": "2020-06-16T22:54:15.075Z",
-//               "updatedAt": "2020-06-16T22:54:15.075Z",
-//               "orderId": 101,
-//               "productId": 7
-//           }
-//       },
-//       {
-//           "id": 2,
-//           "name": "Incredible Plastic Ball",
-//           "price": "145.00",
-//           "description": "Nostrum qui id delectus perspiciatis sit voluptatem et quam rerum.",
-//           "imageUrl": "http://lorempixel.com/640/480/city",
-//           "inventory": 2,
-//           "createdAt": "2020-06-16T22:54:13.693Z",
-//           "updatedAt": "2020-06-16T22:54:13.693Z",
-//           "categoryId": 3,
-//           "OrderDetail": {
-//               "quantity": 2,
-//               "price": "459.00",
-//               "createdAt": "2020-06-16T22:54:15.076Z",
-//               "updatedAt": "2020-06-16T22:54:15.076Z",
-//               "orderId": 101,
-//               "productId": 2
-//           }
-//       },
-//       {
-//           "id": 66,
-//           "name": "Refined Concrete Tuna",
-//           "price": "822.00",
-//           "description": "Nam dolorem libero et a quasi in.",
+//           "productId": 1,
+//           "name": "Awesome Soft Towels",
+//           "price": "247.00",
+//           "description": "Atque et quas laudantium impedit iste.",
 //           "imageUrl": "http://lorempixel.com/640/480/sports",
-//           "inventory": 71,
-//           "createdAt": "2020-06-16T22:54:13.829Z",
-//           "updatedAt": "2020-06-16T22:54:13.829Z",
-//           "categoryId": 5,
-//           "OrderDetail": {
-//               "quantity": 3,
-//               "price": "731.00",
-//               "createdAt": "2020-06-16T22:54:15.076Z",
-//               "updatedAt": "2020-06-16T22:54:15.076Z",
-//               "orderId": 101,
-//               "productId": 66
-//           }
+//           "inventory": 84,
+//           "quantity": 2
+//       },
+//       {
+//           "productId": 5,
+//           "name": "Sleek Cotton Hat",
+//           "price": "586.00",
+//           "description": "Debitis harum placeat ut debitis quis modi aut.",
+//           "imageUrl": "http://lorempixel.com/640/480/nature",
+//           "inventory": 89,
+//           "quantity": 4
+//       },
+//       {
+//           "productId": 3,
+//           "name": "Handmade Plastic Computer",
+//           "price": "343.00",
+//           "description": "Nihil eligendi adipisci voluptatem culpa ipsum quis iusto sunt eius.",
+//           "imageUrl": "http://lorempixel.com/640/480/abstract",
+//           "inventory": 75,
+//           "quantity": 1
+//       },
+//       {
+//           "productId": 7,
+//           "name": "Rustic Soft Shirt",
+//           "price": "785.00",
+//           "description": "Asperiores est esse corporis dicta.",
+//           "imageUrl": "http://lorempixel.com/640/480/food",
+//           "inventory": 59,
+//           "quantity": 3
+//       },
+//       {
+//           "productId": 2,
+//           "name": "Awesome Rubber Mouse",
+//           "price": "710.00",
+//           "description": "Maiores omnis deserunt eos ut alias sed eius maxime.",
+//           "imageUrl": "http://lorempixel.com/640/480/food",
+//           "inventory": 56,
+//           "quantity": 1
+//       },
+//       {
+//           "productId": 66,
+//           "name": "Gorgeous Cotton Tuna",
+//           "price": "880.00",
+//           "description": "In minus quia maxime iusto inventore.",
+//           "imageUrl": "http://lorempixel.com/640/480/abstract",
+//           "inventory": 60,
+//           "quantity": 1
+//       },
+//       {
+//           "productId": 33,
+//           "name": "Practical Soft Mouse",
+//           "price": "435.00",
+//           "description": "Ea ex necessitatibus voluptas consequatur qui voluptates fuga.",
+//           "imageUrl": "http://lorempixel.com/640/480/food",
+//           "inventory": 54,
+//           "quantity": 3
 //       }
 //   ]
 // }
