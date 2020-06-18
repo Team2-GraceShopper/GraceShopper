e1427c1ceff847f9f37778a03db1030ed6ab4f3f
shelilevine <shelisteinberg@gmail.com>
2020-06-18 12:11:34 -0400

    added admin route for creating a new product

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Product, Category} = require('../db/models')
+const {isAdmin} = require('./gatekeeper')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -15,6 +16,19 @@ router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.id)
     res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    if (isAdmin(req.user)) {
+      const product = await Product.create(req.body)
+      res.json(product)
+    } else {
+      res.status(401).send('Unauthorized to create product')
+    }
   } catch (error) {
     next(error)
   }
