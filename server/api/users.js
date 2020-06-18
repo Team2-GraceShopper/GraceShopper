6becd14ebc4490d1b7775ca0d23dd8135450eae1
shelilevine <shelisteinberg@gmail.com>
2020-06-18 15:14:33 -0400

    added route to get all users available to admins

--- server/api/users.js
+++ server/api/users.js
@@ -1,16 +1,19 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const {isAdmin} = require('./gatekeeper')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    if (req.user && isAdmin(req.user)) {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email']
+      })
+      res.json(users)
+    } else res.status(401).send('Unauthorized access to users')
   } catch (err) {
     next(err)
   }
