98d82ff6a708a3c2ea55a7833d8dd303820b5bef
Yang Gu <yg2349@gmail.com>
2020-06-18 09:42:48 -0400

    single product for review and merge

--- client/app.js
+++ client/app.js
@@ -6,10 +6,10 @@ import Routes from './routes'
 const App = () => {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
       <SingleProduct />
       {/* <AllProducts /> */}
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes />
     </div>
   )
--- client/components/SingleProductRender.js
+++ client/components/SingleProductRender.js
@@ -1,42 +1,75 @@
 import React from 'react'
-// import {makeStyles} from '@material-ui/core/styles'
+import {makeStyles} from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Select from '@material-ui/core/Select'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+// import CardActions from '@material-ui/core/CardActions'
+// import CardContent from '@material-ui/core/CardContent'
+// import CardMedia from '@material-ui/core/CardMedia'
 
-// const useStyles = makeStyles(theme => ({
-//     toolbar: {
-//       borderBottom: `1px solid ${theme.palette.divider}`
-//     },
-//     toolbarTitle: {
-//       flex: 1,
-//       alignSelf: 'center',
-//       paddingLeft: '5.5rem'
-//     },
-//     toolbarSecondary: {
-//       justifyContent: 'space-between',
-//       overflowX: 'auto'
-//     },
-//     toolbarLink: {
-//       padding: theme.spacing(1),
-//       flexShrink: 0
-//     }
-//   }))
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  cardMedia: {
+    paddingTop: '56.25%' // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  }
+}))
 
 export default function SingleProductView(props) {
-  // const classes = useStyles()
+  const classes = useStyles()
   let {product} = props
-  console.log('props', props.product)
+
   return product.name ? (
     <div className="single-product">
-      <h1>{product.name}</h1>
+      <h1> {product.name}</h1>
       <img src={product.imageUrl} />
       <h2>${product.price} </h2>
-      <h2>Why You Need It</h2>
+      <h2>Editor's Notes</h2>
       {product.description}
-      <h4>Inventory{product.inventory}</h4>
+      <h4> Only a few left!</h4>
       <Button variant="contained" color="primary">
         ADD TO CART
       </Button>
+
       <Select variant="outlined" color="primary">
         <Option>1</Option>
         <Option>2</Option>
